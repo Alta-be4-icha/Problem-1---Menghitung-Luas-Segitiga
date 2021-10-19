ab104b5f13c1b3ec0a78abefc9b8efefc9508f29
Ichasaja <92065943+Ichasaja@users.noreply.github.com>
2021-10-19 09:33:45 +0700

    Add files via upload

---
+++ luas segitiga yg alas & tinggi ditentukan.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// input
+	var alas float64 = 20
+	var tinggi float64 = 25
+	var luas float64 = (0.5 * alas * tinggi)
+	fmt.Println("Luas segitiga adalah", luas)
+}
